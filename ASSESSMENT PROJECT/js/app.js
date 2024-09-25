d8486658afe80d8d97f21071aa2ae39a405edc4a
Yiling1201 <soireelaine@gmail.com>
2024-09-26 00:31:45 +0200

    Created project

--- ASSESSMENT PROJECT/js/app.js
+++ ASSESSMENT PROJECT/js/app.js
@@ -1,25 +1,58 @@
-//VARIABLES
-  //Buttons
+// VARIABLES
+// Buttons
 let button0 = document.getElementById("button0");
 
-//TEXTS
-let playerRollText= document.getElementById("playerRollText");
+// TEXTS
+let playerRollText = document.getElementById("playerRollText");
+let aiRollText = document.getElementById("aiRollText");
+let resultText = document.getElementById("result");
+let playerScoreText = document.getElementById("playerScoreText");
+let aiScoreText = document.getElementById("aiScoreText");
 
-//DATA
-let playerRoll=0;
+// DATA
+let playerRoll = 0;
+let aiRoll = 0;
+let playerScore = 0;
+let aiScore = 0;
 
-//PROCESSES
+// PROCESSES
 button0.addEventListener("click", function() {
   getRandomNumberOneToSixForPlayer();
   showPlayerRollResult();
+  showAIRollResult();
+  evaluateGameResult();
+  updateScores();
 });
 
-//CONTROLLERS
+// CONTROLLERS
 function getRandomNumberOneToSixForPlayer() {
   playerRoll = Math.floor(Math.random() * 6) + 1;
+  aiRoll = Math.floor(Math.random() * 6) + 1;
 }
 
-//VIEWS
+// Function to evaluate the result (win, lose, or draw)
+function evaluateGameResult() {
+  if (playerRoll > aiRoll) {
+    resultText.innerHTML = "Result: Win!";
+    playerScore++;
+  } else if (playerRoll < aiRoll) {
+    resultText.innerHTML = "Result: Lose!";
+    aiScore++;
+  } else {
+    resultText.innerHTML = "Result: Draw!";
+  }
+}
+
+// VIEWS
 function showPlayerRollResult() {
-  playerRollText.innerHTML = playerRoll;
+  playerRollText.innerHTML = `Player rolled: ${playerRoll}`;
+}
+
+function showAIRollResult() {
+  aiRollText.innerHTML = `AI rolled: ${aiRoll}`;
+}
+
+function updateScores() {
+  playerScoreText.innerHTML = `Player Score: ${playerScore}`;
+  aiScoreText.innerHTML = `AI Score: ${aiScore}`;
 }
