6466f686752ab2a72d5bb9b2e449fe05ec28fee4
Yiling1201 <soireelaine@gmail.com>
2024-09-26 23:33:42 +0200

    Created project

--- ASSESSMENT PROJECT/js/app.js
+++ ASSESSMENT PROJECT/js/app.js
@@ -43,6 +43,7 @@ function evaluateGameResult() {
   }
 }
 
+
 // VIEWS
 function showPlayerRollResult() {
   playerRollText.innerHTML = `Player rolled: ${playerRoll}`;
@@ -56,3 +57,66 @@ function updateScores() {
   playerScoreText.innerHTML = `Player Score: ${playerScore}`;
   aiScoreText.innerHTML = `AI Score: ${aiScore}`;
 }
+
+function saveAsCookie() {
+  document.cookie = "playerScoreHistory=" +playerScore + ";expires=Thu, 29 Dec 2030 12:00:00UTC";
+  document.cookie = "AIScoreHistory=" +aiScore + ";expires=Thu, 29 Dec 2030 12:00:00UTC";
+  document.cookie = "evaluateResult=" +evaluateResult + ";expires=Thu, 29 Dec 2030 12:00:00UTC";
+}
+
+
+function LoadGame() {
+  playerScore = getCookie("playerScore");
+  aiScore = getCookie("aiScore");
+  evaluateResult = getCookie("evaluateResult");
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return 0;
+}
+
+function saveAsCookie() {
+  document.cookie = "playerScoreHistory=" + playerScore + ";expires=Thu, 29 Dec 2030 12:00:00 UTC";
+  document.cookie = "AIScoreHistory=" + aiScore + ";expires=Thu, 29 Dec 2030 12:00:00 UTC";
+  document.cookie = "evaluateResult=" + evaluateResult + ";expires=Thu, 29 Dec 2030 12:00:00 UTC";
+}
+
+function LoadGame() {
+  playerScore = getCookie("playerScoreHistory") || 0; // Default to 0 if not found
+  aiScore = getCookie("AIScoreHistory") || 0; // Default to 0 if not found
+  evaluateResult = getCookie("evaluateResult") || ""; // Default to empty string if not found
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return ""; // Return an empty string if the cookie is not found
+}
+
+// Load game when the page loads
+window.onload = function() {
+  LoadGame();
+  updateScores(); // Update displayed scores based on loaded values
+};
